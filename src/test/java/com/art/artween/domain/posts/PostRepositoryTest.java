de393797ee289b4aa1d15d1409e723214556178d
SMJminjeong <sdwp0920@gmail.com>
2022-12-22 23:06:44 +0900

    JPA 설정 및 H2 DataBase연동 테스트

---
+++ src/test/java/com/art/artween/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.art.artween.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanUp(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void selectPosts(){
+        String title = "테스트 게시글 제목";
+        String content = "테스트 게시글 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("mjsong@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
