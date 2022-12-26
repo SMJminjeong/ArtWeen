eee7038e46d66d77f19967419799f42448014b7e
SMJminjeong <sdwp0920@gmail.com>
2022-12-26 20:42:01 +0900

    JPA 의 @Auditing 을 이용하여 등록일 / 수정일 추가하기

---
+++ src/main/java/com/art/artween/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.art.artween.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+//JPA Entity 클래스들이 상속해서 필드들도 칼럼으로 이닉하도록
+@MappedSuperclass
+//BaseTimeEntity 클래스에 Auditing 기능도 포함
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
--- src/test/java/com/art/artween/domain/PostRepositoryTest.java
+++ src/test/java/com/art/artween/domain/PostRepositoryTest.java
@@ -1,5 +1,7 @@
-package com.art.artween.domain.posts;
+package com.art.artween.domain;
 
+import com.art.artween.domain.posts.Posts;
+import com.art.artween.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,5 +38,20 @@ public class PostRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity(){
+        LocalDateTime now = LocalDateTime.of(2022,12,26,0,0,0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate = "+ posts.getCreatedDate() + ", modifiedDate = "+ posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
